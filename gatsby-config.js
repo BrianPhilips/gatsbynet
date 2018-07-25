a5280db585a11104fc7e3609040eaee432892020
Brian Philips <bphilips@sumo.do>
2018-07-25 11:42:59 -0300

    fixes gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "img",
-        path: `${__dirname}/src/assets`
+        path: `${__dirname}/static/assets`
       }
     },        
     {
